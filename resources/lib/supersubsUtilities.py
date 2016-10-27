a4981cd82b47780dea23990c62cef70cfb23011b
Meir Finkelstine <mfinkelstine@gmail.com>
2016-10-27 22:57:48 +0300

    adding mainsupersub

---
+++ resources/lib/supersubsUtilities.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import cookielib
+import datetime
+import re
+import urllib
+import urllib2
+import zlib
+import json
+
+import bs4
+
+import xbmc
+
+
+class subtitle(object):
+    def __init__(self, ):
+        print "this is a test"
